c964b95844ffd5a616101eae952e755aadb7dfa2
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2023-07-26 01:48:17 -0700

    worked on the header also added a popover

--- linkedln/src/app/home/home.module.ts
+++ linkedln/src/app/home/home.module.ts
@@ -1,3 +1,6 @@
+{
+  /*Disable eslint*/
+}
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
@@ -5,15 +8,11 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { HeaderComponent } from './components/header/header.component';
+import { PopoverComponent } from './components/header/popover/popover.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    HomePageRoutingModule
-  ],
-  declarations: [HomePage]
+  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
+  declarations: [HomePage, HeaderComponent, PopoverComponent],
 })
 export class HomePageModule {}
