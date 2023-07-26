2cc08cb90d31aa26f88786e62636680b3ae65683
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2023-07-26 04:39:51 -0700

    creted the profile summary,start-post,advertising component and added them to the homepage

---
+++ linkedln/src/app/home/components/start-post/start-post.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ModalComponent } from './modal/modal.component';
+
+@Component({
+  selector: 'app-start-post',
+  templateUrl: './start-post.component.html',
+  styleUrls: ['./start-post.component.scss'],
+})
+export class StartPostComponent implements OnInit {
+  @Output() create: EventEmitter<any> = new EventEmitter();
+
+  constructor(public modalController: ModalController) {}
+
+  ngOnInit() {}
+
+  async presentModal() {
+    console.log('CREATE POST');
+    const modal = await this.modalController.create({
+      component: ModalComponent,
+      cssClass: 'my-custom-class2',
+    });
+    await modal.present();
+
+    const { data } = await modal.onDidDismiss();
+    if (!data) return;
+    this.create.emit(data.post.body);
+  }
+
+  // ngOnDestroy() {
+  //   this.userImagePathSubscription.unsubscribe();
+  // }
+}
