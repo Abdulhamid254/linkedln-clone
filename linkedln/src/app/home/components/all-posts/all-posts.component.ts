3cb5edf34cf2b51891982cbc3414faaae605cd89
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2023-07-27 21:40:27 -0700

    created infinite scrolling pagination and consumed it api in the frontend

---
+++ linkedln/src/app/home/components/all-posts/all-posts.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { PostService } from '../../services/post.service';
+import { IonInfiniteScroll } from '@ionic/angular';
+import { Post } from '../../models/Post';
+
+@Component({
+  selector: 'app-all-posts',
+  templateUrl: './all-posts.component.html',
+  styleUrls: ['./all-posts.component.scss'],
+})
+export class AllPostsComponent implements OnInit {
+  // we are going to loop through(ion-infinite-scroll on html) this elememts to ge the infinite scroll
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll | undefined;
+
+  queryParams: string | undefined;
+  allLoadedPosts: Post[] = [];
+  numberOfPosts = 5;
+  skipPosts = 0;
+
+  constructor(private postService: PostService) {}
+
+  ngOnInit() {
+    this.getPosts(false, '');
+  }
+
+  getPosts(isInitialLoad: boolean, event: any) {
+    if (this.skipPosts === 5) {
+      event.target.disabled = true;
+    }
+    this.queryParams = `?limit=${this.numberOfPosts}&skip=${this.skipPosts}`;
+    this.postService.getSelectedPosts(this.queryParams).subscribe(
+      (posts: Post[]) => {
+        for (let post = 0; post < posts.length; post++) {
+          this.allLoadedPosts.push(posts[post]);
+        }
+        if (isInitialLoad) event.target.complete;
+        this.skipPosts = this.skipPosts + 5;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  loadData(event: any) {
+    this.getPosts(true, event);
+  }
+}
