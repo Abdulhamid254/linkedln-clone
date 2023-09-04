b28594d6ac30c6b46bb67ca22ab3273a9971bd36
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2023-09-04 04:27:25 -0700

    Implemented friend req entity & friend req entity

---
+++ api/src/auth/models/friend-request.entity.ts
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+import { UserEntity } from './user.entity';
+import { FriendRequest_Status } from './friend-request.interface';
+
+@Entity('request')
+export class FriendRequestEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => UserEntity, (userEntity) => userEntity.sentFriendRequests)
+  creator: UserEntity;
+
+  @ManyToOne(
+    () => UserEntity,
+    (userEntity) => userEntity.receivedFriendRequests,
+  )
+  receiver: UserEntity;
+
+  @Column()
+  status: FriendRequest_Status;
+}
---
+++ api/src/auth/models/friend-request.interface.ts
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+
+import { User } from './user.interface';
+
+export type FriendRequest_Status = 'Pending' | 'accepted' | 'declined';
+
+export interface FriendRequestStatus {
+  status?: FriendRequest_Status;
+}
+
+export interface FriendRequest {
+  id?: number;
+  creator?: User;
+  receiver?: User;
+  status?: FriendRequest_Status;
+}
