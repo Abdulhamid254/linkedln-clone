64cb5952d7fbcee7b1391ded24fbbcf3c9d161af
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2023-09-07 02:18:31 -0700

    Added thr bsdge for my network

--- linkedln/src/app/home/components/header/header.component.ts
+++ linkedln/src/app/home/components/header/header.component.ts
@@ -1,16 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { PopoverController } from '@ionic/angular';
 import { PopoverComponent } from './popover/popover.component';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { ConnectionProfileService } from '../../services/connection-profile.service';
+import { Subscription, take, tap } from 'rxjs';
+import { FriendRequest } from '../../models/FriendRequest';
+import { FriendRequestsPopoverComponent } from './friend-requests-popover/friend-requests-popover.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
-  constructor(public popoverController: PopoverController) {}
+export class HeaderComponent implements OnInit, OnDestroy {
+  userFullImagePath: string | undefined;
+  private userImagePathSubscription: Subscription | undefined;
 
-  ngOnInit() {}
+  friendRequests: FriendRequest[] | undefined;
+  private friendRequestsSubscription: Subscription | undefined;
+  constructor(
+    public popoverController: PopoverController,
+    private authService: AuthService,
+    public connectionProfileService: ConnectionProfileService
+  ) {}
+
+  ngOnInit() {
+    this.authService
+      .getUserImageName()
+      .pipe(
+        take(1),
+        tap(({ imageName }) => {
+          const defaultImagePath = 'blank-profile-picture.png';
+          this.authService
+            .updateUserImagePath(imageName || defaultImagePath)
+            .subscribe();
+        })
+      )
+      .subscribe();
+
+    this.userImagePathSubscription =
+      this.authService.userFullImagePath.subscribe(
+        (fullImagePath: string | undefined) => {
+          this.userFullImagePath = fullImagePath;
+        }
+      );
+
+    this.friendRequestsSubscription = this.connectionProfileService
+      .getFriendRequests()
+      .subscribe((friendRequests: FriendRequest[]) => {
+        this.connectionProfileService.friendRequests = friendRequests.filter(
+          (friendRequest: FriendRequest) => friendRequest.status === 'pending'
+        );
+      });
+  }
 
   async presentPopover(ev: any) {
     const popover = await this.popoverController.create({
@@ -23,5 +65,23 @@ export class HeaderComponent implements OnInit {
 
     const { role } = await popover.onDidDismiss();
     console.log('onDidDismiss resolved with role', role);
+  }
+
+  async presentFriendRequestPopover(ev: any) {
+    const popover = await this.popoverController.create({
+      component: FriendRequestsPopoverComponent,
+      cssClass: 'my-custom-class',
+      event: ev,
+      showBackdrop: false,
+    });
+    await popover.present();
+
+    const { role } = await popover.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
+  ngOnDestroy() {
+    this.userImagePathSubscription?.unsubscribe();
+    this.friendRequestsSubscription?.unsubscribe();
   }
 }
